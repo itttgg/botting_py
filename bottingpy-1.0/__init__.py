19f2c50bcf4fe87b26ab2611171492b38f20d9b5
itttgg <spdd5724@gmail.com>
2022-02-03 23:35:18 +0300

    edits

--- bottingpy-1.0/__init__.py
+++
@@ -1,8 +0,0 @@
-__name__ = 'bottingpy'
-__description__ = 'Creating bots for you!'
-__version__ = '1.0'
-__company__ = 'Xgame'
-__github__ = 'https://github.com/itttgg/botting_py'
-
-import bottingpy.chat
-import bottingpy.function
