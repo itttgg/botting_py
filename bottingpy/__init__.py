30fe482305c3048a35b3c50a481dfcd24f9c97d2
itttgg <95937737+itttgg@users.noreply.github.com>
2022-02-03 22:28:26 +0300

    1.0

---
+++ bottingpy/__init__.py
@@ -0,0 +1,8 @@
+__name__ = 'bottingpy'
+__description__ = 'Creating bots for you!'
+__version__ = '1.0'
+__company__ = 'Xgame'
+__github__ = 'https://github.com/itttgg/botting_py'
+
+import bottingpy.chat
+import bottingpy.function
